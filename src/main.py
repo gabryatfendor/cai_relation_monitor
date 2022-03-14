31a32b25f90f4a38a3d98340caabb2d319790585
gabryatfendor <sani.gabriele@protonmail.com>
2022-03-14 21:48:00 +0000

    Added base overpass query

---
+++ src/main.py
@@ -0,0 +1,12 @@
+import datetime
+import requests
+
+#setting interval limits
+today = datetime.datetime.now()
+yesterday = today - datetime.timedelta(1)
+
+overpass_today = today.strftime("%Y-%m-%dT%H:%M:%SZ")
+overpass_yesterday = yesterday.strftime("%Y-%m-%dT%H:%M:%SZ")
+
+r = requests.get('http://overpass-api.de/api/interpreter?data=[out:xml][timeout:600];(area(3600042611)->.er;area(3600042472)->.fi;relation(area.er)[\"source\"=\"survey:CAI\"][\"source:ref\"=\"9224011\"];relation(area.fi)[\"source\"=\"survey:CAI\"][\"source:ref\"=\"9224011\"];);(._;>;);out meta geom;')
+print(r.content)
